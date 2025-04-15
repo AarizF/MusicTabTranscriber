f8cf04fbdac8a3ba338adfbe47097a4fa54786cd
AarizF <aarizf@gmail.com>
2025-04-15 20:51:31 +0530

    black installed

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,15 +3,17 @@ import time
 
 app = Flask(__name__)
 
-@app.route('/transcribe', methods=['POST'])
-def transcribe():
-    file = request.files['file']
-    # Process the MP3 file and generate a PDF
-    print('before sleep')
-    time.sleep(5)
-    print('after sleep')
-    # For now, return a dummy PDF file
-    return send_file('dummy.pdf', as_attachment=True)
 
-if __name__ == '__main__':
+@app.route("/transcribe", methods=["POST"])
+def transcribe():
+    file = request.files["file"]
+    # Process the MP3 file and generate a PDF
+    print("before sleep")
+    time.sleep(5)
+    print("after sleep")
+    # For now, return a dummy PDF file
+    return send_file("dummy.pdf", as_attachment=True)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
